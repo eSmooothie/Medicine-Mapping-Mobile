983bbc312682b29252b1df38966765d096739b6f
Sherwin Sandoval <33995402+eSmooothie@users.noreply.github.com>
2021-08-05 08:52:57 +0800

    Setting up Google Map API

--- lib/pages/landing.dart
+++ lib/pages/landing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class LandingPage extends StatefulWidget {
   final String title;
@@ -9,19 +10,23 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: TextButton(
-          child: Text("go to search"),
-          onPressed: () {
-            Navigator.pushNamed(context, "/search");
-          },
-        ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
       ),
     );
   }
